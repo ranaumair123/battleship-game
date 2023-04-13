8c99cd1e6b14e0626a36584ba36202e9bcbdd775
ranaumair123 <rana4874@gmail.com>
2023-04-13 02:00:13 +0000

    game board created

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+import random
+import time
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+letter_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F':5, 'G':6, 'H':7}
+
+def print_board(board):
+    print(' A B C D E F G H')
+    print(' ---------------')
+
+    row_number = 1
+    for row in board:
+        print("%d|%s|" %(row_number, "|".join(row)))
+        row 
