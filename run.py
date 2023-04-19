9b07a0ac522cc8f5ce15573b6b9f61d98f04e560
ranaumair123 <rana4874@gmail.com>
2023-04-19 02:08:49 +0000

    puuted a new grid with computer choice and user input

--- run.py
+++ run.py
@@ -1,46 +1,33 @@
+import random
 
-from random import randint
+grid = [[11,  12,  13,  14,  15,  16,  17,  18],
+        [21,  22,  23,  24,  25,  26,  27,  28],
+        [31,  32,  33,  34,  35,  36,  37,  38],
+        [41,  42,  43,  44,  45,  46,  47,  48],
+        [51,  52,  53,  54,  55,  56,  57,  58],
+        [61,  62,  63,  64,  65,  66,  67,  68],
+        [71,  72,  73,  74,  75,  76,  77,  78],
+        [81,  82,  83,  84,  85,  86,  87,  88]]
+        
 
-game_board = []
+for row in grid:
+    for item in row:
+        print('{:<7}'.format(item), end='')
+    print()
+# Choose a random item from the grid
+random_row = random.choice(grid)
+random_item = random.choice(random_row)
 
-for x in range(5):
-    game_board.append(["O"] * 5)
+# Print the random item
+print(f'The randomly chosen item is: {random_item}')
 
-def print_game_board(game_board):
-    for row in game_board:
-        print(" ".join(row))
+# Ask the user to choose a cell
+user_row = int(input('Enter the row number: '))
+user_col = int(input('Enter the column number: '))
 
-print("Let's play Battleship!")
-print_game_board(game_board)
-
-def random_row(game_board):
-    return randint(0, len(game_board) - 1)
-
-def random_col(game_board):
-    return randint(0, len(game_board[0]) - 1)
-
-ship_row = random_row(game_board)
-ship_col = random_col(game_board)
-# Uncomment the following lines for debugging purposes:
-print(ship_row)
-print(ship_col)
-
-for turn in range(4):
-    print("Turn", turn + 1)
-    guess_row = int(input("Guess Row:"))
-    guess_col = int(input("Guess Col:"))
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship!")
-        break
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Oops, that's not even in the ocean.")
-        elif(game_board[guess_row][guess_col] == "X"):
-            print("You guessed that one already.")
-        else:
-            print("You missed my battleship!")
-            game_board[guess_row][guess_col] = "X"
-        if (turn == 3):
-            print("Game Over")
-        print_game_board(game_board)
+# Check if the user's choice is valid
+if user_row < 1 or user_row > len(grid) or user_col < 1 or user_col > len(grid[0]):
+    print('Invalid input! Row and column numbers must be within range.')
+else:
+    user_choice = grid[user_row-1][user_col-1]
+    print(f'Your choice is: {user_choice}')
